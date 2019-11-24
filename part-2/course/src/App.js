a297d50fd7106143ef45241d83303213c817c078
Xavier <xavier@flowlens.com>
2019-11-24 19:47:59 +0100

    2.1: course contents step6

---
+++ part-2/course/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Part = ({ part }) => {
+  return (
+    <div>{part.name} {part.exercises}</div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return parts.map(part => <Part key={part.id} part={part}/>);
+};
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App;
