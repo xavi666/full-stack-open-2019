d3992aefb4b8587de8a20230109cd423d24b9815
Xavier <xavier@flowlens.com>
2019-11-25 23:29:47 +0100

    2.15: Phonebook step7

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -20,9 +20,12 @@ const App = () => {
       name: newName, 
       number: newNumber
     };
-    setPersons(persons.concat(newPerson));
-    setNewName('');
-    setNewNumber('');
+    axios.post('http://localhost:3001/persons', newPerson)    
+      .then(response => {
+        setPersons(persons.concat(response.data));
+        setNewName('');
+        setNewNumber('');
+      })  
   };
 
   const handleNameChange = (event) => {
