39afa6537b67420b55d55dac35b2c74bc565165c
Xavier <xavier.garces@devex.com>
2019-12-18 15:51:47 +0100

    4.4: helper functions and unit tests, step2

--- part-4/blog-list/tests/list_helper.test.js
+++ part-4/blog-list/tests/list_helper.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper')
+const blogData = require('../utils/dummy_data')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('when list is empty it returns 0', () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([blogData[0]]);
+    expect(result).toBe(blogData[0].likes);
+  });
+  test('when list has multiple blogs equals the sum of likes of each blog', () => {
+    const result = listHelper.totalLikes(blogData);
+    expect(result).toBe(36);
+  });
+  test('when list has blogs with no likes equals the sum of likes of each blog', () => {
+    delete blogData[0].likes; // equals to 7
+    const result = listHelper.totalLikes(blogData);
+    expect(result).toBe(29);
+  });
 })
--- part-4/blog-list/utils/list_helper.js
+++ part-4/blog-list/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  return blogs.reduce((sum,blog) => blog.likes ? sum + blog.likes : sum + 0, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
