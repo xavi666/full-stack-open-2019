3b5be25c7f0cac67b1cd95e8fa0a1ea44c634af5
Xavier <xavier.garces@devex.com>
2019-12-24 20:45:09 +0100

    4.13 Blog list expansions, step1

--- part-4/blog-list/tests/blog_api.test.js
+++ part-4/blog-list/tests/blog_api.test.js
@@ -90,6 +90,25 @@ describe('when there is initially some blogs saved', () => {
     expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
   })
 
+  test('deletion of a blog succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd.length).toBe(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+
 })
 
 afterAll(() => {
