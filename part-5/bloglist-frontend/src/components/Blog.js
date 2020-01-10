50578564d97d5622a71aca02891998c0511b3117
Xavier <xavier.garces@devex.com>
2020-01-10 18:29:43 +0100

    5.6* Blog list frontend, step6

--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if(!expanded) {
+    return (
+      <div
+        onClick={() => setExpanded(true)}
+        style={blogStyle}>
+        {blog.title}
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p onClick={() => setExpanded(false)}>{blog.title}</p>
+      <p>{blog.url}</p>
+      <p>{blog.likes} likes <button onClick={()=>{}}>like</button></p>
+      <p>added by {blog.author}</p>
+    </div>
+  )
+}
 
 export default Blog
