33bb5036a29c3f7121de63bde300a84a1d269e15
Xavier <xavier.garces@devex.com>
2020-02-14 15:30:46 +0100

    6.5*: anecdotes, step3

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -35,7 +35,7 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote
-      )
+      ).sort((a, b)=> b.votes - a.votes)
     default:
       return state
   }
